036376ee3ce4051a25baf355aea55b2cd416e93a
Terryb <terrydagbartels@gmail.com>
2021-12-06 18:35:47 +0000

    content file added

---
+++ src/content.ts
@@ -0,0 +1,12 @@
+export const name = 'Terry Don Bartels'
+
+export const bio = `I Work as a Front-End Web Developer. Love JavaScript, everything 
+ front end and talking about code. Most passionate about creating interactive
+ applications and experiences on the web through the application of Scrum and other 
+ tools that improve efficiency and productivity while keeping the business secure.
+`
+
+export const linkedInUrl = `https://www.linkedin.com/in/terry-bartels-204731125/`
+export const gitHubUrl = `https://github.com/Rynkobes`
+export const twitterUrl = ``
+ 
