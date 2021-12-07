9e2db14c910679444ee2bae3c212c2b32e20c21c
Terryb <terrydagbartels@gmail.com>
2021-12-07 07:55:07 +0000

    container updated with Navbar & About Section

--- src/containers/Portfolio.tsx
+++ src/containers/Portfolio.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Flex } from '@chakra-ui/layout'
 import LandingSection from '../components/LandingSection'
+import NavBar from '../components/NavBar'
+import AboutSection from '../components/AboutSection'
 
 const Portfolio: React.FC = () => {
   return (
    
-      <Flex>
+      <Flex
+        flexDir='column'
+      >
         <Flex 
           h='100vh'
           w='100vw'
         >
           <LandingSection />
         </Flex>
+        <NavBar />
+        <AboutSection />
       </Flex>
    
   )
