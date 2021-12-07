8f8a88c530856d84c3dfb5f43d0979dcebf6fd01
Terryb <terrydagbartels@gmail.com>
2021-12-07 07:55:37 +0000

    NavBar updated.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Flex } from '@chakra-ui/layout'
+import NavItem from './NavItem'
 
 const NavBar = () => {
   return (
-    <Flex>
-      vew
+    <Flex 
+    align='center'
+    justify='center'
+    bgColor='#122349'
+    borderBottom='3px solid #0099ff'
+    >
+      <NavItem link='Home' />
+      <NavItem link='About' />
+      <NavItem link='Portfilio' />
+      <NavItem link='Contact' />
     </Flex>
   )
 }
