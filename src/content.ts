f8cdc452adcfc6fea406bb9d5fd4497daec8197d
Terryb <terrydagbartels@gmail.com>
2021-12-07 07:54:09 +0000

    content added

--- src/content.ts
+++ src/content.ts
@@ -9,4 +9,16 @@ export const bio = `I Work as a Front-End Web Developer. Love JavaScript, everyt
 export const linkedInUrl = `https://www.linkedin.com/in/terry-bartels-204731125/`
 export const gitHubUrl = `https://github.com/Rynkobes`
 export const twitterUrl = ``
- 
+
+export const fastContent = `Fast load times and
+ lag free interaction, my highest priority.`
+
+export const responsiveText = `My layouts will work on any device, big or small.`
+export const intuitiveText = `Strong preference for easy to use, intuitive UX/UI.`
+export const dynamicText = `Websites don't have to be static, I love making pages come to life.`
+
+export const introMessage = `Im a Front-End web Developer who is enthusiastic about learning and solving 
+problems through code. My education, training and experience gives me a strong foundation for web development
+and building complex solutions. I am always excieted to work alongside other amazing 
+engineers and to learn so much more.
+`
