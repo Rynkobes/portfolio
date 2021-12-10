c515e09b74ce244c16de37e24234fbdf5705e848
Terryb <terrydagbartels@gmail.com>
2021-12-10 20:17:45 +0000

    skill card added

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import { Flex, Text, Progress, Box } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
 
 interface SkillProps{
-  skill: string
-  pecentage: number
+  text: string 
 }
 
-const Skill: React.FC = () => {
+const Skill: React.FC<SkillProps> = ({children, text}) => {
   return (
-    <Flex>
-      {/* <Text fontSize='18px' p={2} bgColor='#0099ff' color='#fff' w='120px'>React</Text> */}
-      <Box h='100px'>
-        <Progress value={80} colorScheme='green' borderRadius={8}/>
-      </Box>
+    <Flex 
+      boxShadow='md'
+      flexDir='column'
+      boxSize='90px'
+      w='90px'
+      borderRadius={8}
+      align='center'
+      justifyContent='center'
+      border= '1px solid #bdbdbdbc'
+    >
+      {children}
+      <Text fontSize='13px' mt={1}>{text}</Text>
     </Flex>
   )
 }
