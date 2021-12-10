db2cb3ddff255c1acc8d449d25504616a3c72f3e
Terryb <terrydagbartels@gmail.com>
2021-12-10 20:15:48 +0000

    Nav bar configured with react router

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,11 +9,14 @@ const NavBar = () => {
     justify='center'
     bgColor='#122349'
     borderBottom='3px solid #0099ff'
+    top={0}
+    position='sticky'
     >
-      <NavItem link='Home' />
-      <NavItem link='About' />
-      <NavItem link='Portfilio' />
-      <NavItem link='Contact' />
+      <NavItem link='Home' linkSection='/'/>
+      <NavItem link='About' linkSection='#about'/>
+      <NavItem link='Skills' linkSection='#skills' />
+      <NavItem link='Portfilio' linkSection='#portfolio'/>
+      <NavItem link='Contact' linkSection='#contact'/>
     </Flex>
   )
 }
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import { Link as ReactLink} from 'react-router-dom'
 import {Link} from '@chakra-ui/react'
 
 interface NavProps {
   link: string
+  linkSection: string
 }
 
-const NavItem: React.FC<NavProps> = ({link}) => {
+const NavItem: React.FC<NavProps> = ({link, linkSection}) => {
 
   return (
-    <Link 
+    <Link as={ReactLink}
     color='#fff'
-    
+    to={linkSection}
     p={5}
     _hover={{
       borderBottom: '5px solid #0099ff',
