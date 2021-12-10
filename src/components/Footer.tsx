69234644ac23ec3483cd3e199650c00f3daaf0c5
Terryb <terrydagbartels@gmail.com>
2021-12-10 20:15:25 +0000

    Footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Flex, Text } from '@chakra-ui/layout'
+
+const Footer = () => {
+  return (
+    <Flex
+    align='center'
+    justify='center'
+    bgColor='#122349'>
+      <Text p={4} color='#fff'>&copy; Terry D Bartels 2020</Text>
+    </Flex>
+  )
+}
+
+export default Footer
