384b7ecc09da2e8e3ec7e0cd9147db02141e3bfc
Terryb <terrydagbartels@gmail.com>
2021-12-10 20:13:58 +0000

    Relevant sections ( skillcard, portfolio and footer) added

--- src/containers/Portfolio.tsx
+++ src/containers/Portfolio.tsx
@@ -3,6 +3,9 @@ import { Flex } from '@chakra-ui/layout'
 import LandingSection from '../components/LandingSection'
 import NavBar from '../components/NavBar'
 import AboutSection from '../components/AboutSection'
+import SkillCard from '../components/SkillCard'
+import PortfolioSection from '../components/PortfolioSection'
+import Footer from '../components/Footer'
 
 const Portfolio: React.FC = () => {
   return (
@@ -18,6 +21,9 @@ const Portfolio: React.FC = () => {
         </Flex>
         <NavBar />
         <AboutSection />
+        <SkillCard />
+        <PortfolioSection />
+        <Footer />
       </Flex>
    
   )
